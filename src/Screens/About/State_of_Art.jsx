37166ce220192816711e507baa148ea90a9ccc54
Stanislaus <geraldkhody@gmail.com>
2025-03-28 09:49:46 +0000

    About state-of-art-&-rationale page done

--- src/Screens/About/State_of_Art.jsx
+++ src/Screens/About/State_of_Art.jsx
@@ -1,59 +1,13 @@
 import React from "react";
 import sectionImage1 from "@/assets/cocoa/image1.jpg";
 import sectionImage2 from "@/assets/cocoa/image2.jpg";
-import sectionImage3 from "@/assets/cocoa/image4.jpg";  
+import sectionImage3 from "@/assets/cocoa/image4.jpg";
 
 const State_of_Art = () => {
   return (
-    <div className="wrapper py-20">
-      <div className="flex items-center gap-25  px-10 ">
-        <div className="max-w-[60%] text-justify">
-          <p className=" ">
-            Cocoa (Theobroma cacao L.) is the third highest export commodity of
-            Ghana and represents the most important source of revenue for
-            numerous small scale farmers. Climate change (CC) in the form of
-            higher temperatures and reduced rainfall is expected to adversely
-            affect cocoa productivity and reduce the area suitable for cocoa
-            cultivation in Ghana, but the extent and underlying mechanisms are
-            not well understood. The CLIMCOCOA project aims to develop a
-            comprehensive understanding of the impacts of CC on the
-            socio-biophysical bases of cocoa systems in Ghana, and assess the
-            role of agroforestry (AF) as a model for climate smart agriculture
-            in Ghana. We will use a multidisciplinary approach to investigate
-            the socio-biophysical limitations and options for cocoa cultivation
-            under CC, and to assess institutional and socioeconomic factors that
-            favor or limit adoption by farmers of innovative management options
-          </p>
-          <p className="mt-8">
-            The project is structured in 7 work packages (WPs). WPs 1, 2 and 3
-            provide a comprehensive and novel understanding of cocoa production
-            under current and future climates, using 3 different approaches. WP1
-            studies historical yield and climate data at country/district level,
-            WP2 assesses cocoa yield using on-farm studies over a climate
-            gradient, and WP3 studies cocoa tree ecophysiology. Based on studies
-            of livelihood and farming strategies and institutional mapping, WPs
-            4 and 5 investigate the socioeconomic and institutional factors
-            influencing cocoa farmers’ ability to adapt to CC and adopt AF
-            practices.WP6 is dedicated to capacity building activities and
-            dissemination of results through journal articles, policy briefs,
-            training sessions of trainers, local stakeholder seminars, and a
-            dedicated website, targeting farmers, Government officials and
-            researchers. WP7 is management and coordination. CLIMCOCOA will
-            enhance farmers’ capacity to be more resilient to CC impacts on
-            cocoa in Ghana, and build capacities of local researchers to be able
-            to apply state of the art modelling tools to assess CC impact.
-          </p>
-        </div>
-        <div className="w-[30%]  ">
-          <img src={sectionImage1} alt="" className="rounded-3xl" />
-        </div>
-      </div>
-
-      <div className="flex items-center gap-25 mt-20 px-10 ">
-        <div className="w-[30%]  ">
-          <img src={sectionImage2} alt="" className="rounded-3xl" />
-        </div>
-        <div className="max-w-[60%] text-justify">
+    <div className="wrapper md:pt-10 pb-20">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-15 lg:gap-27 mt-10 sm:mt-15 md:mt-20 px-5 sm:px-10">
+        <div className="w-full md:max-w-[55%] lg:w-[50%] text-justify">
           <p className=" ">
             Ghana and neighboring Côte d’Ivoire produces around 60% of the world
             production of cocoa (Theobroma cacao L.), one of the world’s major
@@ -82,56 +36,84 @@ const State_of_Art = () => {
             effects that CC will have on pests and diseases. Furthermore, these
             simulations 2 on potential cocoa growing areas do not take into
             account the adaptation strategies that farmers will be developing or
-            adopting to cope with CC. Furthermore, literature reviews reveal a
-            surprising lack of knowledge of the response of different cocoa
-            systems to extreme climatic events making predictions uncertain
-            (Zuidema et al., 2005). In addition there is little if any knowledge
-            on adaptive measures in the light of climate change, especially
-            among cocoa communities in Ghana.
+            adopting to cope with CC.
           </p>
-          <p className="mt-8"></p>
+        </div>
+        <div className="w-[70%] sm:w-[50%]  md:w-[37%] lg:w-[30%]  ">
+          <img src={sectionImage1} alt="" className="rounded-3xl" />
         </div>
       </div>
 
-      <div className="flex items-center gap-25 mt-20 px-10 ">
-        <div className="max-w-[60%] text-justify">
+      <div className="flex flex-col-reverse md:flex-row justify-center items-center gap-10 md:gap-15 lg:gap-27 mt-20 px-5 sm:px-10  ">
+        <div className="w-[70%] sm:w-[50%]  md:w-[37%] lg:w-[30%]  ">
+          <img src={sectionImage2} alt="" className="rounded-3xl" />
+        </div>
+        <div className="w-full md:max-w-[55%] lg:w-[50%] text-justify">
           <p className=" ">
-            Cocoa (Theobroma cacao L.) is the third highest export commodity of
-            Ghana and represents the most important source of revenue for
-            numerous small scale farmers. Climate change (CC) in the form of
-            higher temperatures and reduced rainfall is expected to adversely
-            affect cocoa productivity and reduce the area suitable for cocoa
-            cultivation in Ghana, but the extent and underlying mechanisms are
-            not well understood. The CLIMCOCOA project aims to develop a
-            comprehensive understanding of the impacts of CC on the
-            socio-biophysical bases of cocoa systems in Ghana, and assess the
-            role of agroforestry (AF) as a model for climate smart agriculture
-            in Ghana. We will use a multidisciplinary approach to investigate
-            the socio-biophysical limitations and options for cocoa cultivation
-            under CC, and to assess institutional and socioeconomic factors that
-            favor or limit adoption by farmers of innovative management options
-          </p>
-          <p className="mt-8">
-            The project is structured in 7 work packages (WPs). WPs 1, 2 and 3
-            provide a comprehensive and novel understanding of cocoa production
-            under current and future climates, using 3 different approaches. WP1
-            studies historical yield and climate data at country/district level,
-            WP2 assesses cocoa yield using on-farm studies over a climate
-            gradient, and WP3 studies cocoa tree ecophysiology. Based on studies
-            of livelihood and farming strategies and institutional mapping, WPs
-            4 and 5 investigate the socioeconomic and institutional factors
-            influencing cocoa farmers’ ability to adapt to CC and adopt AF
-            practices.WP6 is dedicated to capacity building activities and
-            dissemination of results through journal articles, policy briefs,
-            training sessions of trainers, local stakeholder seminars, and a
-            dedicated website, targeting farmers, Government officials and
-            researchers. WP7 is management and coordination. CLIMCOCOA will
-            enhance farmers’ capacity to be more resilient to CC impacts on
-            cocoa in Ghana, and build capacities of local researchers to be able
-            to apply state of the art modelling tools to assess CC impact.
+            Furthermore, literature reviews reveal a surprising lack of
+            knowledge of the response of different cocoa systems to extreme
+            climatic events making predictions uncertain (Zuidema et al., 2005).
+            In addition there is little if any knowledge on adaptive measures in
+            the light of climate change, especially among cocoa communities in
+            Ghana. Against this background, there is an urgent need to develop
+            climate smart cocoa systems, substantiated by improved knowledge on
+            cocoa eco-physiology, analyses of field data, farmers’ strategies
+            and the surrounding institutional set up. It is increasingly
+            recognized that trees in cocoa systems improve crop production,
+            provide timber and fuel wood, fruits and other products as well as
+            valuable ecosystem services such as carbon sequestration, soil
+            fertility enhancement, and biodiversity conservation (Vaast &
+            Somarriba, 2014). Consequently they can contribute greatly to
+            enhancing food, nutrition and income security of smallholders that
+            produce over 80% of world cocoa. From recent studies in Central
+            America and Indonesia, there is increasing evidence that trees,
+            through microclimatic amelioration, also enhance the resilience of
+            cocoa systems to climate change which is threatening the livelihood
+            of rural cocoa communities globally. Nevertheless, the cocoa sector
+            is mainly advocating an increase in cocoa production through the
+            intensification of cocoa systems mainly via improved germplasm and
+            use of agrochemicals in full sun monoculture, and hence removal of
+            shade trees which decreases smallholders’ ability to cope with price
+            volatility of cocoa, pests and diseases outbreaks and climate
+            change.
           </p>
         </div>
-        <div className="w-[30%]  ">
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-15 lg:gap-27 mt-20 px-5 sm:px-10 ">
+        <div className="w-full md:max-w-[55%] lg:w-[50%] text-justify">
+          <p className=" ">
+            This is also the case in Ghana, where cocoa farms have primarily
+            been managed under low shade or no shade (Gockowski & Sonwa, 2011),
+            as agricultural extension services in Ghana have promoted intensive
+            systems in full sun to maximize short-term yields (Asare, 2005).
+            Today, cocoa yields are declining because of low soil fertility and
+            pest and disease pressures, posing severe challenges to the cocoa
+            sector, from farmers to processors (Ruf, 2011; Tscharntke et al.,
+            2011). Farmers rarely have the financial means to purchase the
+            substantial amounts of fertilizers, fungicides and pesticides needed
+            for enhancing production (Ahenkorah et al. 1987, Ruf & Zadi, 1998),
+            and the notion that full-sun and intensive cocoa systems is superior
+            to other management systems has been questioned. Sun-grown cocoa
+            trees age more rapidly than shaded trees, necessitating a more
+            frequent renewal of the plantation (Jagoret et al., 2011; Gyau et
+            al., 2014). Furthermore, recent research has demonstrated that
+            on-farm yields are often larger in shaded than in non-shaded systems
+            (Asare et al., forthcoming). It seems likely that the traditional
+            agroforests are buffering environments, leading to closer-to-optimum
+            conditions for cocoa, especially under conditions of climate stress,
+            while simultaneously working to increase the carbon storage that can
+            help counter climate change. Furthermore, the need for specific
+            policies addressing CC adaptation in a local context depends on how
+            important the impacts of CC are expected to be and whether farmers
+            can adapt to induced changes in their environment (Burke & Emerick,
+            2013). The bulk of the empirical research on CC impacts on
+            agriculture has focused on field crop agriculture in developed
+            economies with little or no studies on perennial crop such as cocoa
+            in a developing country like Ghana.
+          </p>
+        </div>
+        <div className="w-[70%] sm:w-[50%]  md:w-[37%] lg:w-[30%]   ">
           <img src={sectionImage3} alt="" className="rounded-3xl" />
         </div>
       </div>
